11946dca299e90de21218843b46b63ec61dd6b84
martinabrle <71389217+martinabrle@users.noreply.github.com>
2021-12-17 20:28:51 +0100

    second commit

---
+++ api/models/inventory.js
@@ -0,0 +1,33 @@
+import { Sequelize } from "sequelize";
+import { Word} from "./word.js"
+
+export class Inventory extends Sequelize.Model {
+    static init(sequelize, DataTypes) {
+        return super.init(
+            {
+                id: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true
+                },
+                wordId: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                    references: {
+                        model: Word,
+                        key: 'id'
+                    }
+                },
+                qty: {
+                    type: DataTypes.DECIMAL(10,2),
+                    allowNull: false
+                }
+            }, {
+                sequelize,
+                tableName: "inventory",
+                freezeTableName: true,
+                timestamps: true
+            });
+    }
+}
